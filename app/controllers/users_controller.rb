92ecabc0eb28ac8345a49f9e6c02a0c9fa04e5a4
Madeline Kahn <kahnmv@rose-hulman.edu>
2024-01-29 17:25:25 -0500

    Lab 5 Part 3

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  get '/users/new' do
+    email = session[:sign_up_email] || ""
+    session[:sign_up_email] = nil
+    erb :"users/new.html", locals: {email: email}
+  end
+
+  post '/users' do
+    user = User.new(email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
+    session[:sign_up_email] = nil
+    if not user.save then
+      session[:sign_up_email] = params[:email]
+      flash[:errors] = user.errors.full_messages.join("; ")
+      redirect "/users/new"
+    end
+    flash[:notice] = "Thanks for signing up! You may now log in!"
+    redirect "/"
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
-# class User < ActiveRecord::Base
-#   has_secure_password
-# end
-
-# NOTE: I created this stubbed file for you so that you have an immediate reminder that
-#       you will be using has_secure_password INSTEAD OF trying to store passwords yourself!
-#
+class User < ActiveRecord::Base
+  validates :email, presence: true, uniqueness: true, format: {with: URI::MailTo::EMAIL_REGEXP, message: "is not a valid email address"}
+  has_secure_password
+  validates :password_confirmation, presence: true # because apparently has_secure_password doesn't imply this validation
+end
